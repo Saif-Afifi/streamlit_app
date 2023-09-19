c259c3d99947493dac131e0e05980d52580128f4
Saif-Afifi <66483313+Saif-Afifi@users.noreply.github.com>
2023-09-19 12:34:06 +0300

    app.py

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 
-
 # Title and description
-st.title("Machine Learning Web App")
-st.write("This app uses PyCaret to train and evaluate machine learning models.")
-
-# Dictionary to keep track of trained models
-trained_models = {}
+st.title("Auto Machine Learning Web App")
+st.write("This is an automated app to train and evaluate machine learning models.")
 
 # Sidebar for user input
 st.sidebar.header("Upload Data")
@@ -49,13 +45,13 @@ if uploaded_file is not None:
             setup(data, target=target_column_name, session_id=123)
         else:
             setup(data, target=target_column_name, session_id=123, silent=True, data_split_shuffle=False)
-        
+
         if st.checkbox("Compare Models"):
             compare_models()
 
         # Allow user to choose models
         available_models = models()
-        model_ids = available_models['ID'].tolist()
+        model_ids = available_models.index.tolist()
         model_names = available_models['Name'].tolist()
         selected_models_names = st.multiselect(
             "Select machine learning models to use:",
@@ -66,13 +62,15 @@ if uploaded_file is not None:
         # Get corresponding model IDs for selected model names
         selected_models_ids = [model_id for model_id, model_name in zip(model_ids, model_names) if model_name in selected_models_names]
 
-        # Train and evaluate only the new models
-        for model_id, model_name in zip(selected_models_ids, selected_models_names):
-            if model_name not in trained_models:
-                st.write(f"Training and evaluating {model_name}...")
-                model = create_model(model_id)
-                evaluate_model(model)
-                trained_models[model_name] = model  # Add the trained model to the dictionary
+        # Check if any new models have been added
+        new_models_ids = [model_id for model_id in selected_models_ids if model_id not in st.session_state.get('trained_models', [])]
+        st.session_state.trained_models = selected_models_ids
+
+        # Train and evaluate the selected models
+        for model_id, model_name in zip(new_models_ids, selected_models_names):
+            st.write(f"Training and evaluating {model_name}...")
+            model = create_model(model_id)
+            evaluate_model(model)
 
     # Make Predictions
     st.sidebar.header("Make Predictions")
@@ -84,18 +82,17 @@ if uploaded_file is not None:
             new_data = pd.read_csv(uploaded_file)
             st.write("New data for predictions:")
             st.write(new_data.head())
-            for model_name, model in trained_models.items():
-                predictions = predict_model(model, data=new_data)
-                st.write(f"Predictions using {model_name} (Model ID: {model_ids[model_names.index(model_name)]}):")
+            for model_id, model_name in zip(selected_models_ids, selected_models_names):
+                predictions = predict_model(model_id, data=new_data)
+                st.write(f"Predictions using {model_name} (Model ID: {model_id}):")
                 st.write(predictions)
 
     # Save Model
     st.sidebar.header("Save Model")
     if st.sidebar.button("Save Model"):
-        final_model = trained_models.get(selected_models_names[0])  # Use the first selected model for saving
-        if final_model:
-            save_model(final_model, 'final_model')
-            st.write("Model saved as 'final_model.pkl'.")
+        final_model = create_model(selected_models_ids[0])  # Use the first selected model for saving
+        save_model(final_model, 'final_model')
+        st.write("Model saved as 'final_model.pkl'.")
 
 # Footer
 st.sidebar.write("By Saif Gamal")
